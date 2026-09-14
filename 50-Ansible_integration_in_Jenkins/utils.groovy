916c1f8104b6245fee1267b453b578aa62e21f62
Senol Erdogan <senol@mac.local>
2026-09-13 23:45:51 -0500

    debigging synamic host parsing

--- 50-Ansible_integration_in_Jenkins/utils.groovy
+++ 50-Ansible_integration_in_Jenkins/utils.groovy
@@ -14,7 +14,7 @@ def getAnsibleServerIp(){
                 script: "ansible-inventory -i $SUBDIR/ansible/dyn-hosts.aws_ec2.yaml --host tag_AnsibleGroup_ansible_controller | python3 -c \"import sys, json; print(json.load(sys.stdin)['public_dns_name'])\"",
                 returnStdout: true
             ).trim()
-            // echo "Ansible Controller Public DNS: ${public_dns}"
+            echo "Ansible Controller Public DNS: ${public_dns}"
         }
 }
 
